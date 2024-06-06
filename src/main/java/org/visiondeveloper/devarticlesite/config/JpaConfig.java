43f7e193a8779d6c20024db17a4ffc3b6db05a5f
Cjungwo <jwoo516@naver.com>
2024-06-06 18:12:52 +1000

    #8 - set application properties
    * jpa auditing function
    * apply boot property
    * TODO: expose password --> hidden

---
+++ src/main/java/org/visiondeveloper/devarticlesite/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package org.visiondeveloper.devarticlesite.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("tester"); // TODO: - update after implement spring security auth function
+    }
+}
