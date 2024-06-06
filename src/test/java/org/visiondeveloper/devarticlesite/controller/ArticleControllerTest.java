295736aaa3efa64b6e6df270a029e85af67e09c1
Cjungwo <jwoo516@naver.com>
2024-06-07 01:20:03 +1000

    #12 - add ArticleController and test
    for control api of articles

---
+++ src/test/java/org/visiondeveloper/devarticlesite/controller/ArticleControllerTest.java
@@ -0,0 +1,77 @@
+package org.visiondeveloper.devarticlesite.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View Controller - Articles")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("WIP")
+    @DisplayName("[view][GET] Articles page - 200")
+    @Test
+    void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Disabled("WIP")
+    @DisplayName("[view][GET] Article page - 200")
+    @Test
+    void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+    }
+
+    @Disabled("WIP")
+    @DisplayName("[view][GET] Article Search page - 200")
+    @Test
+    void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
+    }
+
+    @Disabled("WIP")
+    @DisplayName("[view][GET] Hashtag Search page - 200")
+    @Test
+    void givenNothing_whenRequestingHashtagSearchView_thenReturnsHashtagSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"));
+    }
+
+}
