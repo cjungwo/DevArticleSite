291ca724ea1316bb8e8dfe7fafdbb653d682351a
Cjungwo <jwoo516@naver.com>
2024-06-06 19:11:09 +1000

    #4 - data rest settng
    annotation setting to repositories

--- src/main/java/org/visiondeveloper/devarticlesite/repository/ArticleRepository.java
+++ src/main/java/org/visiondeveloper/devarticlesite/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package org.visiondeveloper.devarticlesite.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.visiondeveloper.devarticlesite.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
