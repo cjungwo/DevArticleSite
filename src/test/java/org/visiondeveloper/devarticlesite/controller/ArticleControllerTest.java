7797bd5aeeb21ed6612526ad97082619871be298
Cjungwo <jwoo516@naver.com>
2024-06-07 01:47:14 +1000

    #12 - basic articles view
    make basic articles view for test success

--- src/test/java/org/visiondeveloper/devarticlesite/controller/ArticleControllerTest.java
+++ src/test/java/org/visiondeveloper/devarticlesite/controller/ArticleControllerTest.java
@@ -3,6 +3,7 @@ package org.visiondeveloper.devarticlesite.controller;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,11 +16,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ArticleControllerTest {
     private final MockMvc mvc;
 
-    public ArticleControllerTest(MockMvc mvc) {
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
-    @Disabled("WIP")
     @DisplayName("[view][GET] Articles page - 200")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
