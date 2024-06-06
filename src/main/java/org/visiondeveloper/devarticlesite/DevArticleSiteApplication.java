10ae2118a39abfed206e175aeab34e057172bf67
Cjungwo <jwoo516@naver.com>
2024-06-06 11:43:23 +1000

    #1 - project setting for start
    make new spring boot project, connect github & gitkraken and set .gitignore file

---
+++ src/main/java/org/visiondeveloper/devarticlesite/DevArticleSiteApplication.java
@@ -0,0 +1,13 @@
+package org.visiondeveloper.devarticlesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevArticleSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevArticleSiteApplication.class, args);
+	}
+
+}
