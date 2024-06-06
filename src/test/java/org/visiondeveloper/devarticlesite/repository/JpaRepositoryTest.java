fbcfd8957abb5442abcededf25cbdcfae032ca5f
Cjungwo <jwoo516@naver.com>
2024-06-06 18:16:05 +1000

    #8 - Jpa Test
    crud test and cascading test

---
+++ src/test/java/org/visiondeveloper/devarticlesite/repository/JpaRepositoryTest.java
@@ -0,0 +1,96 @@
+package org.visiondeveloper.devarticlesite.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.visiondeveloper.devarticlesite.config.JpaConfig;
+import org.visiondeveloper.devarticlesite.domain.Article;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA Connection Test")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("Select Test")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
+
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(300);
+
+    }
+
+    @DisplayName("Insert Test")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        // Given
+        long previousCount = articleRepository.count();
+        Article article = Article.of("new title", "new content", "new hashtag");
+
+        // When
+        Article savedArticle = articleRepository.save(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+
+    }
+
+    @DisplayName("Update Test")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String newTitle = "new title";
+        article.setTitle(newTitle);
+
+        // When
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+        // Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("title", newTitle);
+
+    }
+
+    @DisplayName("Delete Test")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        long deletedCommentSize = article.getArticleComments().size();
+
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentSize);
+
+    }
+
+}
