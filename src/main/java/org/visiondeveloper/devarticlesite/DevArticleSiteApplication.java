d3115451f88ccaa02f813eb3036edf9e693c9c47
Cjungwo <jwoo516@naver.com>
2024-06-07 13:51:04 +1000

    #12 - add spring boot configuration processor dependency
    for thymeleaf decoupled logic

--- src/main/java/org/visiondeveloper/devarticlesite/DevArticleSiteApplication.java
+++ src/main/java/org/visiondeveloper/devarticlesite/DevArticleSiteApplication.java
@@ -2,7 +2,9 @@ package org.visiondeveloper.devarticlesite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class DevArticleSiteApplication {
 
